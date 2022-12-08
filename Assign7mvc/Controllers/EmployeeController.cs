d5e292b563ae7ae44698fc85158bb93dc6629e10
ajaydev-ts <ajaydev.ts@gadgeon.com>
2022-12-08 16:55:16 +0530

    first commit

---
+++ Assign7mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,93 @@
+﻿using Assign7mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Assign7mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Employee Employee { get; set; }
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;    
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Employee = new Employee();
+            if(id== null)
+            {
+                //create
+                return View(Employee);
+            }
+            //update
+            Employee = _db.Employees.FirstOrDefault(u => u.Id == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()   //bind ind so no need obj
+        {
+            if (ModelState.IsValid)
+            {
+                if (Employee.Id == 0)
+                {
+                    //create
+                    _db.Employees.Add(Employee);
+                }
+                else
+                {
+                    _db.Employees.Update(Employee);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Employee);
+        }
+
+        #region API Calls
+
+
+
+        /*public class EmployeeController : Controller
+        {
+            private readonly ApplicationDbContext _db;
+
+            public EmployeeController(ApplicationDbContext db)
+            {
+                _db = db;
+            }
+    */
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Employees.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employeeFromDb = await _db.Employees.FirstOrDefaultAsync(u => u.Id == id);
+            if (employeeFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Employees.Remove(employeeFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+    #endregion
+    
+}
---
+++ Assign7mvc/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assign7mvc.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+    }
+}
